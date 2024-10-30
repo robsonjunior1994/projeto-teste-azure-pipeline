4dd19904ecf483d002ef8a97d8e7a2398cae4ce2
Robson Junior <robsonjunior1994@gmail.com>
2024-10-30 16:17:17 -0300

    Primeiro commit.

---
+++ ApiDeTeste/ApiDeTeste/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDeTeste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "API Online 16h16min" });
+        }
+    }
+}
